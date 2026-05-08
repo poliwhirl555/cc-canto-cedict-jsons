8244b5300b8398870eb58d1eec417e49e743c193
poliwhirl555 <lesterlinproject@gmail.com>
2026-05-07 23:59:09 -0700

    Oops, forgot to add the parser file to git.

---
+++ parser.py
@@ -0,0 +1,39 @@
+# I think I have to write the parser myself, so I guess this is the file for it
+
+# CC-Canto
+# Split by whitepsace, first is trad, second is simplified, third is pinyin, fourth is jyutping
+# Need to figure out how to deal with entries
+# Should be every "; " splits into a new entry, so splitting by space is already good, but I can already see a typo in there
+
+# Can source parsing code from here: https://github.com/aaronhktan/jyut-dict/blob/main/src/dictionaries/cedict/generate-readings.py
+# Just have to remember to include the MIT License
+# Do I even need to since I'm taking a subportion of the code and not the entire thing? Maybe I should just to be safe.
+
+# Use Array, JSON< or SQL/some other sort of database? 
+# Need to store the dictionaries in readable form somehow else it'll be annoying to have to run this each time.
+
+# At this point, might as well create JSON and SQLite database version of the data, with an update script, so that
+# I don't have to deal with this every time I want to access the data programmatically 
+
+import sqlite3
+
+# Parsing code sourced from: Jyut Dictionary
+# https://github.com/aaronhktan/jyut-dict/blob/main/src/dictionaries/cedict/generate-readings.py
+
+def parse_cc_canto(file):
+ with open(file, "r", encoding="utf8") as f:
+        for line in f:
+            if len(line) == 0 or line[0] == "#":
+                continue
+
+            split = line.split()  # Splits by whitespace
+            traditional = split[0]
+            simplified = split[1]
+            pinyin = line[line.index("[") + 1 : line.index("]")].lower().replace("v", "u:")
+            jyutping = line[line.index("{") + 1 : line.index("}")].lower()
+            
+            # Need custom parsing for entries, since I do actually want to store that information
+
+            # Split by "\", find last "\"?
+            # defintion = line[line.index("\")]
+
