6a5e1aee0b7395e4453354ffe2c974d2b347bb60
poliwhirl555 <lesterlinproject@gmail.com>
2026-05-09 23:40:01 -0700

    Added stub for future CC-EDICT parsing function. Moved the surnames toggle to that function.

--- parser.py
+++ parser.py
@@ -52,8 +52,6 @@ def parse_cc_canto(file, surnames = True):
 
             entry = {"traditional": traditional, "simplified": simplified, "pinyin": pinyin, "jyutping": jyutping, "definitions": definitions}
             
-            
-
 
             # Block to handle hanzi with multiple pronounciations and entries, like 重, which has 4 entries.
             # Converts into bucket if something hashes into the same key, else add normally
@@ -67,7 +65,48 @@ def parse_cc_canto(file, surnames = True):
 
     return entries
 
+# Stub for a future parse CC-EDICT function. Moved the surname skipping toggle here since it'll probably be needed for CC-EDICT
+def parse_cc_edict(file, surnames = True):
+    entries = {}
+    with open(file, "r", encoding="utf8") as f:
 
+        for line in f:
+            if len(line) == 0 or line[0] == "#":
+                continue
+
+            # Unmodified parser, adapt for CC_EDICT
+
+            # split = line.split()  # Splits by whitespace
+            # traditional = split[0]
+            # simplified = split[1]
+            # pinyin = line[line.index("[") + 1 : line.index("]")].lower().replace("v", "u:")
+            # jyutping = line[line.index("{") + 1 : line.index("}")].lower()
+
+            # definitions = line[line.index("/") + 1 : line.rindex("/")].split("/")
+
+            # Added a toggle for surnames, with the default being true as to capture a full scope of the dictionary
+            # Skip surname if surname toggle is false and the entry has surname in it's definition
+
+            # if not surnames and "surname" in definitions:
+            #     continue
+
+            # entry = {"traditional": traditional, "simplified": simplified, "pinyin": pinyin, "jyutping": jyutping, "definitions": definitions}
+            
+            
+
+
+            # Block to handle hanzi with multiple pronounciations and entries, like 重, which has 4 entries.
+            # Converts into bucket if something hashes into the same key, else add normally
+
+            # if not entries.get(traditional):
+            #     entries[traditional] = entry
+            # elif type(entries[traditional]) is list:
+            #     entries[traditional].append(entry)
+            # else:
+            #     entries[traditional] = [entries[traditional], entry]
+            
+
+    return entries
 
 
 
