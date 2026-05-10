d5cf5b356bbf573bf4aa113bbef04c6f27c3a211
poliwhirl555 <lesterlinproject@gmail.com>
2026-05-09 23:33:13 -0700

    Added a surname exclusion toggle, although I checked the test entries and it might not be necessary. Also changed the name of the test input.

--- parser.py
+++ parser.py
@@ -23,7 +23,7 @@ from typing import List
 
 # Input: CC-Canto file
 # Output: A (k,v) map of v = dicts containing all the information in an entry, k = the traditional character
-def parse_cc_canto(file):
+def parse_cc_canto(file, surnames = True):
     entries = {}
     with open(file, "r", encoding="utf8") as f:
 
@@ -36,10 +36,24 @@ def parse_cc_canto(file):
             simplified = split[1]
             pinyin = line[line.index("[") + 1 : line.index("]")].lower().replace("v", "u:")
             jyutping = line[line.index("{") + 1 : line.index("}")].lower()
+
+            # Seems like there are python style comments marked by # in the defintions that need handling (see the test input file)
+            # This takes care of the comments since it leaves off everything after the last part of the defintion
+            # The comments might be useful when trying to merge definitions with the CC-EDICT ones, since the comments say which
+            # entries were adapted from CC-EDICT
             definitions = line[line.index("/") + 1 : line.rindex("/")].split("/")
+
+            # Added a toggle for surnames, with the default being true as to capture a full scope of the dictionary
+            # Skip surname if surname toggle is false and the entry has surname in it's definition
+            # Hmm, actually this might not work since the surname is added as a definition within the definitions, and not as something separate
+            # Might be good though, since I can just keep it in and not have to worry about this
+            if not surnames and "surname" in definitions:
+                continue
+
             entry = {"traditional": traditional, "simplified": simplified, "pinyin": pinyin, "jyutping": jyutping, "definitions": definitions}
             
-            # Should still probably skip surnames though
+            
+
 
             # Block to handle hanzi with multiple pronounciations and entries, like 重, which has 4 entries.
             # Converts into bucket if something hashes into the same key, else add normally
@@ -52,6 +66,10 @@ def parse_cc_canto(file):
             
 
     return entries
+
+
+
+
 
             
 
