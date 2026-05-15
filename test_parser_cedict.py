b47d83fad1e1d8fc6eae16f8b814dccb3a2354dc
poliwhirl555 <lesterlinproject@gmail.com>
2026-05-14 17:24:47 -0700

    Added some tests for the V2 CEDICT format.

---
+++ test_parser_cedict.py
@@ -0,0 +1,21 @@
+import pytest
+from parser import parse_cc_cedict
+
+def test_single_entry_single_def_v2():
+    expected = {"時常": {"traditional": "時常",  "simplified": "时常", "pinyin": "shi2chang2", 
+                    "definitions": ["often; frequently"]}}
+    assert parse_cc_cedict("test-input-files/cc-cedict/single line single def v2.txt")
+
+def test_single_entry_multi_def_v2():
+    expected = {"是": {"traditional": "是", "simplified": "是", "pinyin": "shi4",
+                    "definitions": ["to be (followed by substantives only)", "correct; right; true", "(respectful acknowledgement of a command) very well", "(adverb for emphatic assertion)"]}}
+    assert parse_cc_cedict("test-input-files/cc-cedict/single line multi def v2.txt")
+
+def test_mixed_v1_v2():
+    expected = {"昭": {"traditional": "昭",  "simplified": "昭", "pinyin": "zhao1", 
+                    "definitions": ["bright", "clear", "manifest", "to show clearly"]},
+                "是": {"traditional": "是", "simplified": "是", "pinyin": "shi4",
+                    "definitions": ["to be (followed by substantives only)", "correct; right; true", "(respectful acknowledgement of a command) very well", "(adverb for emphatic assertion)"]},
+                "時常": {"traditional": "時常",  "simplified": "时常", "pinyin": "shi2chang2", 
+                    "definitions": ["often; frequently"]}}
+    assert parse_cc_cedict("test-input-files/cc-cedict/mixed v1 v2.txt") == expected
