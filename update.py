b9191498498b630dfd71e937ab47b1b116d0636a
poliwhirl555 <lesterlinproject@gmail.com>
2026-05-18 01:39:18 -0700

    Fixed a few bugs: - Accidentally using the type None instead of stringified None for string concatenation of the filenames, resulting in errors. - Thought glob and regex were the same thing (they're not), and also learned glob() doesn't support curly braces, so used the wrong syntax for the old file cleanup detection. - json.dump() kept putting in the Hnazi as escaped unicode. Changed the mode to ensure_ascii = false, hopefully that doesn't cause any problems.
    It should work now, although I still need to maybe write some tests for it just to ensure and as a method to make sure future refactoring doesn't break things.

--- update.py
+++ update.py
@@ -7,8 +7,11 @@ from zipfile import *
 from pathlib import Path
 from parser import DICT_TYPES, VALID_KEYS, parse
 
-# read_time = time.strptime(filename[filename.index("2") : filename.index(".")], "%Y-%m-%d_%H:%M_%Z")
-# print(f"read time: {read_time}")
+# TODO: Current issues:
+# - Blocked at line 69 of update when generating jsons. Doesn't seem to wrok wtih cedict but does work with CC-Canto
+# - Not cleaning up the old files properly, at least not the json files.
+# - Encoding of the jsons does not properly render the Hanzi, only as escaped unicode, which doesn't quite matter as it should read properly, but is still annoying
+
 
 GET_LINKS = {"CEDICT": "https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.zip",
              "CANTO": "https://cantonese.org/cccanto-170202.zip"}
@@ -35,7 +38,7 @@ def fetch_raw():
         savefile = Path(filename)
         r = requests.get(GET_LINKS[dt], timeout = 30)
         savefile.write_bytes(r.content)
-        raw_paths.append[savefile]
+        raw_paths.append(savefile)
     return raw_paths
 
 # Function to delete all raw files, usually used to remove the old ones
@@ -66,13 +69,18 @@ def generate_jsons(input_file_path):
     for key in VALID_KEYS[dt]:
         current_time = time.strftime("%Y-%m-%d", time.gmtime())
         dict_data = parse(filename, dict_type, key)
-        storage_name = filename + "_key_" + key + "_" + current_time +".json"
+        truncated_filename = filename[:filename.rindex(".")]
+        storage_name = truncated_filename + "_key_" + str(key) + "_" + current_time +".json"
         with open(storage_name, "w") as out_file:
-            json.dump(dict_data, out_file, indent = 4)
+            # ensure_ascii as false makes the Hanzi human readable, but hopefully it doesn't cause any problems
+            json.dump(dict_data, out_file, ensure_ascii = False, indent = 4)
 
 def clean_jsons():
-    search_regex = f"*({DICT_TYPES[0]}|{DICT_TYPES[1]})*.json"
-    results = glob.glob(search_regex)
-    for file in results:
-        os.remove(file)
+    # search_regex = f".*({DICT_TYPES[0].lower()}|{DICT_TYPES[1].lower()}).*\.json"
+    # Need this "for" because fmatch through glob doesn't support the multiple option curly brace
+    for dt in DICT_TYPES:
+        search_glob = f"*{dt.lower()}*.json"
+        results = glob.glob(search_glob)
+        for file in results:
+            os.remove(file)
     
