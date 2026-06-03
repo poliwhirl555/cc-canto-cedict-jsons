936be4409bc51ee565907d5268090900b899094e
poliwhirl555 <lesterlinproject@gmail.com>
2026-06-03 12:22:57 -0700

    Updated parser.py for parsing readings, still need to add the splice code. Updated LICENSE to include the readings' license.

--- py_cc_dicts/parser.py
+++ py_cc_dicts/parser.py
@@ -1,9 +1,10 @@
 # import sqlite3
 
 # These being in capitals causes so much grief I should really just change them to lowercase
-DICT_TYPES = ["CEDICT", "CANTO"]
-VALID_KEYS = {DICT_TYPES[0]: ["traditional", "simplified", "pinyin", "definitions", None],
-               DICT_TYPES[1]: ["traditional", "simplified", "pinyin", "jyutping", "definitions", None]}
+DICT_TYPES = ["CEDICT", "CANTO", "READINGS"]
+VALID_KEYS = {DICT_TYPES[0]: ["traditional", "simplified", "pinyin", "jyutping", "definitions", None],
+                DICT_TYPES[1]: ["traditional", "simplified", "pinyin", "jyutping", "definitions", None],
+                DICT_TYPES[2]: ["traditional", "simplified", "pinyin", None]}
 
 # Parsing code based on code in Jyut Dictionary
 # https://github.com/aaronhktan/jyut-dict/blob/main/src/dictionaries/cedict/generate-readings.py
@@ -53,14 +54,17 @@ def parse(filepath, dict_type, key = None):
             # This takes care of the comments since it leaves off everything after the last part of the defintion
             # The comments might be useful when trying to merge definitions with the CC-EDICT ones, since the comments say which
             # entries were adapted from CC-EDICT
-            definitions = line[line.index("/") + 1 : line.rindex("/")].split("/")
+            definitions = []
+            if not dict_type == DICT_TYPES[2]:
+                definitions = line[line.index("/") + 1 : line.rindex("/")].split("/")
 
             entry = {"traditional": traditional, "simplified": simplified, "pinyin": pinyin}
 
             if dict_type == DICT_TYPES[1]:
                 entry["jyutping"] = jyutping
 
-            entry["definitions"] = definitions
+            if not dict_type == DICT_TYPES[2]:
+                entry["definitions"] = definitions
 
             storage = None
             if key == "definitions":
@@ -114,6 +118,18 @@ def parse_cc_cedict(filepath, key = "traditional", surnames = True) -> (list[dic
     """
     # Might need to change this later if I add surname exclusion
     return parse(filepath, DICT_TYPES[0], key)
+
+def parse_readings(filepath, key = "traditional") -> dict:
+    """
+    Parse provided CC-CEDICT Jyutping reading file into a JSON dict keyed with input key.
+
+    Returns either a list of dicts cointaining reading entries if key is None, or a dict containing entries of (key, entry dict).
+
+    Args:
+        filepath (str): Path to text or u8 file containing raw dictionary data
+        key (str): Valid key as defined in parser.py, to key the output JSON with
+    """
+    return parse(filepath, DICT_TYPES[2], key)
 
 # Maybe add a surname remover at some point
 # And or some SQLite integration later as well, for easier searching
