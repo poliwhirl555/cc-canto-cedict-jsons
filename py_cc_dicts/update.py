7e028008b9e3f3d8f71431a5a23e491309f3241b
poliwhirl555 <lesterlinproject@gmail.com>
2026-06-04 14:13:45 -0700

    Made get_jsons() more robust by using the actual file prefixes that they were generated with instead of lazily using the dict_type, which means that they don't accidentally catch other files nearly as easily.

--- py_cc_dicts/update.py
+++ py_cc_dicts/update.py
@@ -158,10 +158,15 @@ def get_jsons(dir = "", dict_type = ""):
 
     jsons = []
     if dict_type:
-        jsons = glob.glob(f"*{dict_type.lower()}*.json")
+        # Recreate the creation of the file prefix in generate_jsons, so it doesn't duplicate or catch other files as easily
+        int_name = INTERNAL_NAME[dict_type]
+        file_prefix = int_name[ : int_name.rindex(".")]
+        jsons = glob.glob(f"{file_prefix}*.json")
     else:
         for dt in DICT_TYPES:
-            jsons.extend(glob.glob(f"*{dt.lower()}*.json"))
+            int_name = INTERNAL_NAME[dt]
+            file_prefix = int_name[ : int_name.rindex(".")]
+            jsons.extend(glob.glob(f"{file_prefix}*.json"))
 
     # Need to do this in order to make sure it's an absolute path to the correct location
     jsons = list(map(lambda jp : str(Path(jp).absolute().resolve()), jsons))
